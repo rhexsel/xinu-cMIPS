cdf82a412781153a727eedfaf484ef8a59bfc726
Roberto Hexsel <roberto@inf.ufpr.br>
2017-06-07 11:28:54 -0300

    another error with the tty

--- xinu/system/main.c
+++ xinu/system/main.c
@@ -1,45 +1,26 @@
 /*  main.c  - main */
 
 #include <xinu.h>
+#include <ramdisk.h>
+
+extern process shell(void);
 
 int fibonacci(int n);
 
-#if 0
 int prA(void);
 int prB(void);
+int echo(void);
+
+int32 n; // shared variable
+
+// #define PROC_VOID FALSE
+
 int prod(sid32, sid32);
 int cons(sid32, sid32);
-int echo(void);
-#endif
 
-int fibo(void);
-int tinp(int32);
+#define EOT 0x04
 
-
-#define BSZ   (1<<10) // 1K, MUST be a power of 2
-#define EOT   0x04
-
-volatile int  rxhd;
-volatile int  rxtl;
-volatile int  txhd;
-volatile int  txtl;
-
-char rxb[BSZ];
-char txb[BSZ];
-
-
-// convert small integer (i<16) to hexadecimal digit
-static inline unsigned char i2c(int i) {
-  return ( ((i) < 10) ? ((i)+'0') : (((i & 0x0f)+'a')-10) );
-}
-
-// convert hexadecimal digit to integer (i<16)
-static inline unsigned int c2i(char c) {
-  return ( ((c) <= '9') ? ((c)-'0') : (((c)-'a')+10) );
-}
-
-
-#include "fib_vet.h"
+sid32 produced, consumed; // shared variable
 
 /************************************************************************/
 /*									*/
@@ -48,163 +29,60 @@ static inline unsigned int c2i(char c) {
 /************************************************************************/
 
 int main(void) {  // int argc, char **argv) {
-  int i, j, fib, pid_fibo, pid_tinp;
 
-  // remove echo-ing of input chars, remove \d de "\n\d"
-  // control( CONSOLE, TC_NOECHO, 0,0);
-  control( CONSOLE, TC_MODER, 0,0);
+  int i, j, fibo, pid_cons, pid_prod;
+  n = 0;
 
-  rxhd = rxtl = 0;
-  txhd = txtl = 0;
+  consumed = semcreate(0);  //semaphore
+  produced = semcreate(1);
+  // kprintf("sem c=%x q(%x)  p=%x q(%x)\n", consumed, semtab[consumed].squeue,
+  // 	  produced, semtab[produced].squeue);
 
-  // create(ender, espaco na pilha, prior, nome, num argumentos)
-#if 1
-  if ( (pid_fibo = create(fibo, 4096, 31, "fibo", 0)) == SYSERR )
-    kprintf("err cre fibo\n");
+  //create(ender, espaco na pilha, prior, nome, num argumentos)
+#if 0
+  if ( (pid_prod = create(prod, 4096, 20, "prod", 2,consumed,produced))
+       == SYSERR )
+    kprintf("err cre prod\n");
 
-  if ( (pid_tinp = create(tinp, 4096, 30, "tinp", 1, pid_fibo)) == SYSERR )
-    kprintf("err cre tinp\n");
+  if ( (pid_cons = create(cons, 4096, 20, "cons", 2,consumed,produced))
+       == SYSERR )
+    kprintf("err cre cons\n");
 
-  kprintf("pid fibo=%x tinp=%x\n", pid_fibo, pid_tinp);
+  kprintf("pid cons=%x prod=%x\n", pid_cons, pid_prod);
 
-  if ( resume(pid_tinp) == SYSERR ) kprintf("err res tinp\n");
-  if ( resume(pid_fibo) == SYSERR ) kprintf("err res fibo\n");
+  if ( resume(pid_cons) == SYSERR ) kprintf("err res cons\n");
+  if ( resume(pid_prod) == SYSERR ) kprintf("err res prod\n");
 #endif
 
-  kprintf("\nmain\n");
+#if 1
+  if ( resume(create(prA, 4096, 30, "pr_A", 0)) == (pri16)SYSERR )
+    kprintf("err pr_A\n");
+  
+  if ( resume(create(prB, 4096, 31, "pr_B", 0)) == (pri16)SYSERR )
+    kprintf("err prB\n");
+#endif
+
+#if 1
+  if ( resume(create(echo, 4096, 35, "echo", 0)) == (pri16)SYSERR )
+    kprintf("err echo\n");
+#endif  
+
+
+  kprintf("%s\n", "main()");
 
 #if 0
   while (TRUE) {
 #else
-  for (j = 0; j < 7; j++) {
+  for (j=0; j < 5; j++) {
 #endif    
-    for (i = 1; i < 46; i++) {
-      fib = fibonacci(i);
-      kprintf("."); // , fib);
-      if (i == 45) {
-	kprintf(" %x\n", fib);
-	sleep(1);
-      }
+    for (i = 0; i < 45; i++) {
+      fibo = fibonacci(i);
+      kprintf("%-6x\n", fibo);
     }
   }
   return OK;
-} //-------------------------------------------------------------------
 
-
-
-//
-// reads string from TTY, sends integer for conversion do Fibonacci number
-//
-int tinp(int32 pid_fibo) {
-  char c, e;
-  int num, terminus;
-  volatile int last;
-
-  kprintf("\n\ttinp B\n");
-
-  last = 0;
-  terminus = FALSE;
-
-  do {
-    e = getc(CONSOLE);
-    if (e != 0x0d) {
-      // putc(CONSOLE, e);
-      rxb[rxtl] = e;
-      rxtl = (rxtl+1) & (BSZ-1);
-    
-      if (last <= rxtl) {
-	c = rxb[last];
-	last = (last+1) & (BSZ-1);
-      
-	if (c == EOT) {
-	  terminus = TRUE;
-	} else {  // convert str to int
-	  if (c == '\n') {
-	    num = 0;
-	    while ( rxb[rxhd] != '\n' ) {  // string to int
-	      num = num << 4;
-	      num = c2i( rxb[rxhd] ) + num;
-	      rxhd = (rxhd + 1) & (BSZ-1);
-	    }
-	    rxhd = (rxhd + 1) & (BSZ-1); // jump over \n
-
-	    if (num >= D_SZ) {                            // end simulation
-	      kprintf("\n\n\t\tWRONG INPUT %x >= %x\n\n",num, D_SZ);
-	      return(SYSERR);
-	    }
-	    kprintf("\tsend %x\n", num);
-	    while ( send(pid_fibo, num) == SYSERR ) {
-	      sleep(1);
-	    }
-	  }
-	}
-      }
-    }
-  } while ( terminus == FALSE );  // end of transmission?
-
-  while ( send(pid_fibo, -1) == SYSERR ) {
-    sleep(1);
-  }
-
-  kprintf("\n\ttinp E\n");
-
-  return(OK);
-
-} //----------------------------------------------------------------
-
-
-//
-// receives integer, converts to Fibonacci number, sends it through TTY
-//
-int fibo(void) {
-  int32 f, fib, num, places;
-
-  kprintf("\n\tfibo B\n");
-
-  do {
-    num = receive();
-    kprintf("\tfibo %x\n", num);
-    if (num != -1) {
-
-      fib = dat[num];               // int to Fibonacci
-
-      kprintf("\tf[%x]=%x\n", num, fib);
-
-      places = 28;                  // Fibonacci to string
-      while ( places > -1 ) {
-	f = fib;
-	f = (f>>places) & 0xf;
-	txb[txtl] = (char)i2c(f);
-	txtl = (txtl + 1) & (BSZ-1);
-	places = places - 4;
-      }
-      txb[txtl] = (char)'\n';
-      txtl = (txtl + 1) & (BSZ-1);
-
-      if (txhd < txtl) {            // RX is faster than TX
-	putc( CONSOLE, txb[txhd] );
-	txhd = (txhd + 1) & (BSZ-1);
-      }
-    }
-  } while (num != -1);
-
-  txb[txtl] = (char)EOT;    // end of transmission
-  txtl = (txtl + 1) & (BSZ-1);
-
-  while ( (txhd < txtl) ) {
-    putc( CONSOLE, txb[txhd] );
-    txhd = (txhd + 1) & (BSZ-1);
-  }
-
-  // putc( CONSOLE, (char)EOT );
-
-  delay_us(64);    // wait for last char to go out
-
-  kprintf("\n\tfibo E\n");
-
-  return(OK);
-} //---------------------------------------------------------------
-
+} //------------------------------------------------------------------
 
 
 int fibonacci(int32 n) {
@@ -224,44 +102,7 @@ int fibonacci(int32 n) {
     f2 = fi;
   }
   return fi;
-} //-------------------------------------------------------------------
-
-
-#if 0
-
-#if 0
-  if ( resume(create(echo, 4096, 35, "echo", 0)) == (pri16)SYSERR )
-    kprintf("err echo\n");
-#endif  
-
-int echo() {
-  char c;
-  int  i;
-
-  kprintf("%s\n", "echo");
-
-  i = 0;
-  do {
-    c = getc(CONSOLE);
-    putc(CONSOLE, c);
-    i += 1;
-  } while (c != EOT);
-
-  kprintf("%x\n", i);
-
-  return(i);
-} //--------------------------------------------------------------------
-
-
-
-#if 1
-  if ( resume(create(prA, 4096, 30, "pr_A", 0)) == (pri16)SYSERR )
-    kprintf("err pr_A\n");
-  
-  if ( resume(create(prB, 4096, 31, "pr_B", 0)) == (pri16)SYSERR )
-    kprintf("err prB\n");
-#endif
-
+} //----------------------------------------------------------------------
 
 
 int prA(){
@@ -272,7 +113,7 @@ int prA(){
     i += 1;
   }
   return(i);
-}
+} //----------------------------------------------------------------------
 
 int prB(){
   int i = 0;
@@ -282,11 +123,7 @@ int prB(){
     i += 1;
   }
   return(i);
-}
-
-
-
-
+} //----------------------------------------------------------------------
 
 //consumidor
 int cons(sid32 consumed, sid32 produced) {
@@ -302,7 +139,7 @@ int cons(sid32 consumed, sid32 produced) {
   }
   kprintf("\tcons end\n");
   return(i);
-}
+} //----------------------------------------------------------------------
 
 //produtor
 int prod(sid32 consumed, sid32 produced) {
@@ -318,7 +155,23 @@ int prod(sid32 consumed, sid32 produced) {
   }
   kprintf("\tprod end\n");
   return(i);
-}
+} //----------------------------------------------------------------------
 
 
-#endif
+int echo() {
+  char c;
+  int  i;
+
+  kprintf("%s\n", "echo()");
+
+  i = 0;
+  do {
+    c = getc(CONSOLE);
+    putc(CONSOLE, c);
+    i += 1;
+  } while (c != EOT);
+
+  kprintf("%x\n", i);
+
+  return(i);
+} //----------------------------------------------------------------------
